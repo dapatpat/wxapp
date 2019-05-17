9151e905a6294f72f9f2ba67e4bde4f572114c5e
dapatpat <836010221@qq.com>
2019-05-17 15:26:38 +0800

    add apidoc

---
+++ apiDoc.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * 1)获取表的全部记录, 可用于初始化
+ ****GET
+ *   http://10.3.15.96/api/init_page?paramerslist=[Swiper,PageConfig,Good,Orde]
+ *   paramerslist:包含数据表名的列表,返回对应表的全部记录
+ *   
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
