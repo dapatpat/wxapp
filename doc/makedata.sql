876b6ed400cd1b8fb555dc55f9d66be753b8e6c1
dapatpat <836010221@qq.com>
2019-05-20 10:56:25 +0800

    add  model data

--- doc/makedata.sql
+++ doc/makedata.sql
@@ -8,15 +8,15 @@ insert into Article values("7","ArticleName7","7777-77-77","7","7","ArticleAutho
 insert into Article values("8","ArticleName8","8888-88-88","8","8","ArticleAuthor8","ArticleDetail8","ArticleIntroduc8","ArticleRemark8","8","8888-88-88","8888-88-88");
 insert into Article values("9","ArticleName9","9999-99-99","9","9","ArticleAuthor9","ArticleDetail9","ArticleIntroduc9","ArticleRemark9","9","9999-99-99","9999-99-99");
 
-insert into Good values("1","1","食品","GoodSaleType1","食品","辣条","GoodSubtitle1","GoodCharac1","GoodIntroduct1","GoodOrigiPrice1","GoodSaleCount1","1","1","GoodDetail1","1","1","1","1","GoodRemark1","1");
-insert into Good values("2","2","食品","GoodSaleType1","食品","薯条","GoodSubtitle2","GoodCharac2","GoodIntroduct2","GoodOrigiPrice2","GoodSaleCount2","2","2","GoodDetail2","2","2","2","2","GoodRemark2","2");
-insert into Good values("3","3","生活用品","GoodSaleType2","生活用品","沐浴露","GoodSubtitle3","GoodCharac3","GoodIntroduct3","GoodOrigiPrice3","GoodSaleCount3","3","3","GoodDetail3","3","3","3","3","GoodRemark3","3");
-insert into Good values("4","4","生活用品","GoodSaleType2","生活用品","洗发水","GoodSubtitle4","GoodCharac4","GoodIntroduct4","GoodOrigiPrice4","GoodSaleCount4","4","4","GoodDetail4","4","4","4","4","GoodRemark4","4");
-insert into Good values("5","5","工具","GoodSaleType3","工具","螺丝刀","GoodSubtitle5","GoodCharac5","GoodIntroduct5","GoodOrigiPrice5","GoodSaleCount5","5","5","GoodDetail5","5","5","5","5","GoodRemark5","5");
-insert into Good values("6","6","工具","GoodSaleType3","工具","菜刀","GoodSubtitle6","GoodCharac6","GoodIntroduct6","GoodOrigiPrice6","GoodSaleCount6","6","6","GoodDetail6","6","6","6","6","GoodRemark6","6");
-insert into Good values("7","7","服务类商品","GoodSaleType4","服务类商品","送货上门","GoodSubtitle7","GoodCharac7","GoodIntroduct7","GoodOrigiPrice7","GoodSaleCount7","7","7","GoodDetail7","7","7","7","7","GoodRemark7","7");
-insert into Good values("8","8","服务类商品","GoodSaleType5","服务类商品","代客泊车","GoodSubtitle8","GoodCharac8","GoodIntroduct8","GoodOrigiPrice8","GoodSaleCount8","8","8","GoodDetail8","8","8","8","8","GoodRemark8","8");
-insert into Good values("9","9","食品","GoodSaleType6","食品","鱼皮","GoodSubtitle9","GoodCharac9","GoodIntroduct9","GoodOrigiPrice9","GoodSaleCount9","9","9","GoodDetail9","9","9","9","9","GoodRemark9","9");
+insert into Good values(1,"1","食品","GoodSaleType1","食品","辣条","GoodSubtitle1","GoodCharac1","GoodIntroduct1","GoodOrigiPrice1","GoodSaleCount1","1","1","GoodDetail1","1","1","1","1","GoodRemark1","1");
+insert into Good values(2,"2","食品","GoodSaleType1","食品","薯条","GoodSubtitle2","GoodCharac2","GoodIntroduct2","GoodOrigiPrice2","GoodSaleCount2","2","2","GoodDetail2","2","2","2","2","GoodRemark2","2");
+insert into Good values(3,"3","生活用品","GoodSaleType2","生活用品","沐浴露","GoodSubtitle3","GoodCharac3","GoodIntroduct3","GoodOrigiPrice3","GoodSaleCount3","3","3","GoodDetail3","3","3","3","3","GoodRemark3","3");
+insert into Good values(4,"4","生活用品","GoodSaleType2","生活用品","洗发水","GoodSubtitle4","GoodCharac4","GoodIntroduct4","GoodOrigiPrice4","GoodSaleCount4","4","4","GoodDetail4","4","4","4","4","GoodRemark4","4");
+insert into Good values(5,"5","工具","GoodSaleType3","工具","螺丝刀","GoodSubtitle5","GoodCharac5","GoodIntroduct5","GoodOrigiPrice5","GoodSaleCount5","5","5","GoodDetail5","5","5","5","5","GoodRemark5","5");
+insert into Good values(6,"6","工具","GoodSaleType3","工具","菜刀","GoodSubtitle6","GoodCharac6","GoodIntroduct6","GoodOrigiPrice6","GoodSaleCount6","6","6","GoodDetail6","6","6","6","6","GoodRemark6","6");
+insert into Good values(7,"7","服务类商品","GoodSaleType4","服务类商品","送货上门","GoodSubtitle7","GoodCharac7","GoodIntroduct7","GoodOrigiPrice7","GoodSaleCount7","7","7","GoodDetail7","7","7","7","7","GoodRemark7","7");
+insert into Good values(8,"8","服务类商品","GoodSaleType5","服务类商品","代客泊车","GoodSubtitle8","GoodCharac8","GoodIntroduct8","GoodOrigiPrice8","GoodSaleCount8","8","8","GoodDetail8","8","8","8","8","GoodRemark8","8");
+insert into Good values(9,"9","食品","GoodSaleType6","食品","鱼皮","GoodSubtitle9","GoodCharac9","GoodIntroduct9","GoodOrigiPrice9","GoodSaleCount9","9","9","GoodDetail9","9","9","9","9","GoodRemark9","9");
 
 insert into swiper values(1,"SwiperUrl1","1","SwiperTypeName1","SwiperIsFlat1","SwiperCreateTime1");
 insert into swiper values(2,"SwiperUrl2","2","SwiperTypeName2","SwiperIsFlat1","SwiperCreateTime2");
@@ -27,3 +27,37 @@ insert into swiper values(6,"SwiperUrl6","6","SwiperTypeName6","SwiperIsFlat1","
 insert into swiper values(7,"SwiperUrl7","7","SwiperTypeName7","SwiperIsFlat1","SwiperCreateTime7");
 insert into swiper values(8,"SwiperUrl8","8","SwiperTypeName8","SwiperIsFlat1","SwiperCreateTime8");
 insert into swiper values(9,"SwiperUrl9","9","SwiperTypeName9","SwiperIsFlat1","SwiperCreateTime9");
+
+
+insert into Say values(1 ,"SayType" ,1 ,"SayDetail1" ,"SayRemark1" ,"SayCreateTime1" ,1);
+insert into Say values(2 ,"SayType" ,1 ,"SayDetail2" ,"SayRemark2" ,"SayCreateTime2" ,2);
+insert into Say values(3 ,"SayType" ,2 ,"SayDetail3" ,"SayRemark3" ,"SayCreateTime3" ,3);
+insert into Say values(4 ,"SayType" ,2 ,"SayDetail4" ,"SayRemark4" ,"SayCreateTime4" ,4);
+insert into Say values(5 ,"SayType" ,3 ,"SayDetail5" ,"SayRemark5" ,"SayCreateTime5" ,5);
+insert into Say values(6 ,"SayType" ,3 ,"SayDetail6" ,"SayRemark6" ,"SayCreateTime6" ,6);
+insert into Say values(7 ,"SayType" ,2 ,"SayDetail7" ,"SayRemark7" ,"SayCreateTime7" ,7);
+insert into Say values(8 ,"SayType" ,1 ,"SayDetail8" ,"SayRemark8" ,"SayCreateTime8" ,8);
+insert into Say values(9 ,"SayType" ,0 ,"SayDetail9" ,"SayRemark9" ,"SayCreateTime9" ,9);
+
+
+insert into GoodGoodStyleRela values(1,1,1,"GGSRemark1","GGSCreateTime1");
+insert into GoodGoodStyleRela values(2,2,2,"GGSRemark2","GGSCreateTime2");
+insert into GoodGoodStyleRela values(3,3,3,"GGSRemark3","GGSCreateTime3");
+insert into GoodGoodStyleRela values(4,4,4,"GGSRemark4","GGSCreateTime4");
+insert into GoodGoodStyleRela values(5,5,5,"GGSRemark5","GGSCreateTime5");
+insert into GoodGoodStyleRela values(6,6,6,"GGSRemark6","GGSCreateTime6");
+insert into GoodGoodStyleRela values(7,7,7,"GGSRemark7","GGSCreateTime7");
+insert into GoodGoodStyleRela values(8,8,8,"GGSRemark8","GGSCreateTime8");
+insert into GoodGoodStyleRela values(9,9,9,"GGSRemark9","GGSCreateTime9");
+
+
+
+insert into GoodStyle values(1,1,"GStyleTypeName1","GStyleDetail1","GStyleRemark1","GStyleCreateTime1");
+insert into GoodStyle values(2,2,"GStyleTypeName2","GStyleDetail2","GStyleRemark2","GStyleCreateTime2");
+insert into GoodStyle values(3,3,"GStyleTypeName3","GStyleDetail3","GStyleRemark3","GStyleCreateTime3");
+insert into GoodStyle values(4,4,"GStyleTypeName4","GStyleDetail4","GStyleRemark4","GStyleCreateTime4");
+insert into GoodStyle values(5,5,"GStyleTypeName5","GStyleDetail5","GStyleRemark5","GStyleCreateTime5");
+insert into GoodStyle values(6,6,"GStyleTypeName6","GStyleDetail6","GStyleRemark6","GStyleCreateTime6");
+insert into GoodStyle values(7,7,"GStyleTypeName7","GStyleDetail7","GStyleRemark7","GStyleCreateTime7");
+insert into GoodStyle values(8,8,"GStyleTypeName8","GStyleDetail8","GStyleRemark8","GStyleCreateTime8");
+insert into GoodStyle values(9,9,"GStyleTypeName9","GStyleDetail9","GStyleRemark9","GStyleCreateTime9");
